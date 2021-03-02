3a5f503763b45374298ae99477bbcd5507f757ef
PaolkaT <48362178+PaolkaT@users.noreply.github.com>
2021-03-03 01:30:42 +0400

    Add task 4 and task 6

--- src/main/scala/task.scala
+++ src/main/scala/task.scala
@@ -1,5 +1,9 @@
 import java.io.{File, PrintWriter}
 
+import sun.security.util.Length
+
+import scala.annotation.tailrec
+import scala.collection.immutable.ListMap
 import scala.io.Source
 
 object task {
@@ -28,8 +32,11 @@ object task {
     // подсчёт частоты
     val finalString = groupedString
       .map(x => x._1 -> BigDecimal(x._2.toDouble / len.toDouble).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble)
+      .filter(x => x._2 != 0.000)
+    // для красивого вывода
+    val sortedMap = ListMap(finalString.toSeq.sortWith(_._2 > _._2):_*)
     // вывод в файл
-    writeInFile("table_task_1.csv", finalString)
+    writeInFile("table_task_1.csv", sortedMap)
   }
 
   def task_2() = {
@@ -40,7 +47,7 @@ object task {
     // цикл для нахождения пар подряд идущих элементов
     var allPairs = List((filteredString(0), filteredString(1)))
     for (i <- 1 to filteredString.length - 2) {
-       allPairs = (filteredString(i), filteredString(i + 1)) :: allPairs
+      allPairs = (filteredString(i), filteredString(i + 1)) :: allPairs
     }
     val ident = allPairs.groupBy(identity).view.mapValues(_.length).toMap
     val len = filteredString.length
@@ -48,13 +55,18 @@ object task {
     val finalString = ident
       .map(x => x._1._1.toString + x._1._2.toString ->
         BigDecimal(x._2.toDouble / len.toDouble).setScale(3, BigDecimal.RoundingMode.HALF_UP).toDouble)
+      .filter(x => x._2 != 0.000)
+    // для красивого вывода
+    val sortedMap = ListMap(finalString.toSeq.sortWith(_._2 > _._2):_*)
     // вывод в файл
-    writeInFile("table_task_2.csv", finalString)
+    writeInFile("table_task_2.csv", sortedMap)
   }
 
-  def task_3(a: String, n: Int, k: Int) = {
-    val alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя!@#$%^&*(),.?-1234567890 "
-    val procText = a.toLowerCase
+  def task_3(n: Int, k: Int) = {
+    val alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя!@#$%^&*(),.?-1234567890 \n"
+    val readFromFile = Source.fromFile("text_for_task")
+    val stringForProcess = readFromFile.mkString
+    val procText = stringForProcess.toLowerCase
     val m = alphabet.length // length
     // кодирование
     val cod = procText.map(x => (alphabet.indexOf(x) * n + k) % m) // .map(x => b((m + x) % m))
@@ -66,21 +78,55 @@ object task {
       .map(x =>
         if (alphabet.indexOf(x) - k >= 0) (alphabet.indexOf(x) - k) % m
         else (m + alphabet.indexOf(x) - k) % m)
-    print(decod)
+    //    print(decod)
     println("Декодирование:")
     val decodChar = decod.map(x => alphabet.charAt(x)).mkString
     println(decodChar)
+  }
+
+  @tailrec
+  def makeTable(count: Int = 32, tableVigenere: List[String] = List(),
+                alphabet: String = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"): List[String] = tableVigenere match {
+    case tbl =>
+      if (count >= 0) {
+        // что же это такое? первая мапа кодирует со сдвигом 1. Вторая мапа меняет позиции в алфавите на буквы
+        makeTable(count - 1,
+          alphabet
+            .map(x => (alphabet.indexOf(x) + count) % alphabet.length).map(x => alphabet.charAt(x)).mkString :: tbl)
+      }
+      else tbl
+  }
+
+  def task_4(keyword: String) = {
+    val alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя!@#$%^&*(),.?-1234567890 \n"
+
+    val tabulaRecta = makeTable()
+
+    val readFromFile = Source.fromFile("text_for_task")
+    val stringForProcess = readFromFile.mkString.toLowerCase
+    val charToIndexCod = stringForProcess.map(x => alphabet.indexOf(x))
+    val cod = charToIndexCod.zipWithIndex.map{ case (x, i) =>
+      if (x <= 32) tabulaRecta(x).charAt(alphabet.indexOf(keyword.charAt(i % keyword.length)))
+      else alphabet.charAt(x)}
+    println("Зашифрованное сообщение: " + cod.mkString)
+
+    val charToIndexDecod = cod.map(x => alphabet.indexOf(x))
+    val decod = charToIndexDecod.zipWithIndex.map{ case (x, i) =>
+      if (x <= 32) {
+        tabulaRecta(alphabet.indexOf(keyword.charAt(i % keyword.length))).indexOf(alphabet.charAt(x))
+      }
+      else x}
+    val decodChar = decod.map(x => alphabet.charAt(x))
+    println("Декодированное сообщение: " + decodChar.mkString)
   }
 
   def binToDec(string: String): Int = {
     Integer.parseInt(string, 2)
   }
 
-  def task_5() = {
-    // отладочный ключ
-    val key = "101010"
+  def task_5(key: String) = {
     // алфавит и его кодирование
-    val alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя!(),.?-1234567890 "
+    val alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя!(),.?-1234567890 \n"
     // (0.toString * (6 - alphabet.indexOf(x).toBinaryString.length)) нужно для добавления нулей слева
     val binaryCodes = alphabet
       .map(x => (0.toString * (6 - alphabet.indexOf(x).toBinaryString.length)) + alphabet.indexOf(x).toBinaryString)
@@ -89,7 +135,6 @@ object task {
     val stringForProcess = readFromFile.mkString.toLowerCase
     // соответствие букв и кода
     val codeInputText = stringForProcess.map(x => binaryCodes(alphabet.indexOf(x)))
-    println(codeInputText)
     // непосредственно xor с ключом
     val xor = codeInputText.map(x => (binToDec(x) ^ binToDec(key)).toBinaryString)
     val xorChars = xor.map(x => alphabet.charAt(binToDec(x) % alphabet.length))
@@ -103,10 +148,31 @@ object task {
     println("Сообщение после декодирования: " + decodChars.mkString)
   }
 
+  def generateKey(keyword: List[Int], output: String = "")
+                 (count: Int = 0,
+                  length: Int = (scala.math.pow(2, keyword.length) - 1).toInt): (List[Int], String) = keyword match {
+    case head :: tail =>
+      if (count < length) {
+        val newElem = head ^ tail.last
+        generateKey(newElem :: (List(head) ::: tail.init), output + tail.last.toString)(count + 1)
+      }
+      else {
+        (keyword, output)
+      }
+    case _ => (keyword, output)
+  }
+
+  def task_6(keyword: List[Int]) = {
+    val key = generateKey(keyword)()._2
+    task_5(key)
+  }
+
   def main(args: Array[String]): Unit = {
 //    task_1()
 //    task_2()
-    task_5()
-//    task_3("Я зашифровал здесь что-то", 1, 2)
+//    task_3(1, 3)
+//    task_4("тест")
+//    task_5("101010")
+    task_6(List(0, 0, 1))
   }
 }
